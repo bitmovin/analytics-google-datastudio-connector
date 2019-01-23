ad3264976d0818511c6d325d77d961c5d93f804f
Roland Grießer <office@rolandgriesser.at>
2019-01-23 15:20:39 +1300

    Updated comments

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-/******************************************************************************
- * Copyright (C) 2019, bitmovin GmbH, All Rights Reserved                    *
- *                                                                           *
- * Created on: 25.08.2015                                                    *
- * Author: Wolfram Hofmeister   <wolfram.hofmeister@bitmovin.net>            *
- *                                                                           *
- * This source code and its use and distribution, is subject to the terms    *
- * and conditions of the applicable license agreement.                       *
- *****************************************************************************/
+// Copyright 2019, bitmovin GmbH
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+// of the Software, and to permit persons to whom the Software is furnished to do 
+// so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
  /**
   * Returns the available dimensions the user can use to query or group the API results.
   * 
-  * @returns {String[]} Array of the available dimensions.
+  * @returns {Array} Array of the available dimensions.
   */
 function getDimensions() {
   if(this.dimensions === undefined) {
